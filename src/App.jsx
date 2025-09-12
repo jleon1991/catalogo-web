e366a85e4cdfe5972414047569123b36dd70e3b0
Jeremy <jjelc1991@gmail.com>
2025-09-11 23:22:31 -0500

    Primer commit - Catálogo web

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+<div className="h-2 bg-red-500"></div>
+
+import React from "react";
+import CatalogoWeb from "./CatalogoWeb";
+
+export default function App() {
+  return (
+    <div className="min-h-screen">
+      <CatalogoWeb />
+    </div>
+  );
+}
